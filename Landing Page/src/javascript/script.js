e261d05ed0f96694ae0dbb4acdd2bb27d89e766e
israelgoncalvesx <97671966+israelgoncalvesx@users.noreply.github.com>
2025-04-12 18:29:42 -0300

    Add files via upload

---
+++ Landing Page/src/javascript/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#mobile-btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile-btn').toggleClass('fa-x');
+    });
+
+});
